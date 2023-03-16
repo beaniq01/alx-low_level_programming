208e867df5374b657584181c3cb7409670031707
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 14:40:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 /**
  * main - 101-quote.c
- * Description -Write a c program line followed by a new line, to the standard error
+ * Description - program line followed by a new line, to the standard error
  * return :Always 0(success)
  */
 int main(void)
 {
-        write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
+        write(2,
+	      "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
+	      59);
        	return (1);
 }
