8a58d8343f996ffc38a99170525018c34f972ba7
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 15:57:37 -0700

    101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 /**
  * main - 101-quote.c
  * Description - program line followed by a new line, to the standard error
- * return :Always 0(success)
+ * return :0 always(success)
  */
 int main(void)
 {
-        write(2,
-	      "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
+write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
 	      59);
-       	return (1);
+return (1);
 }
