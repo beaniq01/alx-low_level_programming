12ef0e7bbdc3652c1727af2e0a3c737edbaca637
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 04:41:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/**
+ * main - using  printf instead of puts 
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
