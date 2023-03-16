b7a540506497d9c68da5a16fb7973ba589fb1636
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 05:30:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main()
 {
-	char char type
-	int int type
-	long int long int type
-	long long int long long int type
+	char char type;
+	int int type;
+	long int long int type;
+	long long int long long int type;
 printf("size of char:%zu byte\n", sizeof(charType));
 printf("size of int:%zu byte\n", sizeof(intType));
 printf("size of long int :%zu byte\n", sizeof(longintType));
