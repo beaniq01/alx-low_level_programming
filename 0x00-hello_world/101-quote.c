022cdca26cfd3d7583c785a9624a264e67999416
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 16:01:01 -0700

    101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 /**
- * main - 101-quote.c
+ * main - Entry point
  * Description - program line followed by a new line, to the standard error
  * return :0 always(success)
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
+write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
 	      59);
 return (1);
 }
