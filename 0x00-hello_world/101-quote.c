1f2082b1b189e79a2460a24e034247c19c9b8320
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 14:37:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <unistd.h>
 /**
  * main - 101-quote.c
- * Description -Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error
+ * Description -Write a c program line followed by a new line, to the standard error
  * return :Always 0(success)
  */
 int main(void)
 {
-        write(2,
-	      "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
-	      59);
-
-        return (1);
+        write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
+       	return (1);
 }
