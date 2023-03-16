84ae24a09098bfd39057ff249186a7e7110df8a0
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 05:27:42 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints size of various types
+ * Return : 0 (success)
+ */
+int main()
+{
+	char char type
+	int int type
+	long int long int type
+	long long int long long int type
+printf("size of char:%zu byte\n", sizeof(charType));
+printf("size of int:%zu byte\n", sizeof(intType));
+printf("size of long int :%zu byte\n", sizeof(longintType));
+printf("size of long long int:%zu byte\n", sizeof(longlongintType));
+return 0;
+}
