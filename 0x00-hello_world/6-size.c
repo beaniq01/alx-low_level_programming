902c3fe6aab317955d848b16b09b7d97d1809235
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 05:41:29 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("size of char:%zu byte\n", sizeof(a));
-printf("size of int:%zu byte\n", sizeof(b));
-printf("size of long int :%zu byte\n", sizeof(c));
-printf("size of long long int:%zu byte\n", sizeof(d));
-printf("size of float:%zu byte\n", sizeof(f));
+printf("size of char:%zu byte(s)\n", sizeof(a));
+printf("size of int:%zu byte(s)\n", sizeof(b));
+printf("size of long int :%zu byte(s)\n", sizeof(c));
+printf("size of long long int:%zu byte(s)\n", sizeof(d));
+printf("size of float:%zu byte(s)\n", sizeof(f));
 return 0 ;
 }
