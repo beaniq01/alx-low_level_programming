ee605f532e2bb1187043ea7b2476c454176be85a
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 13:12:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unstd.h>
+#include <unistd.h>
 /**
  * main - Entry
  * return : 1
  */
 int main (void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58)
-	return (1)
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
