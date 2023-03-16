d8b95f84b55f69937a605c6888f11e2e276571d7
beaniq01 <beatricewambui6065@gmail.com>
2023-03-16 14:29:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
- * return : 1 if success
+ * main - 101-quote.c
+ * Description -Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error
+ * return :Always 0(success)
  */
 int main(void)
 {
-        write(1,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+        write(2,
+	      "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",
+	      59);
+
         return (1);
 }
-
